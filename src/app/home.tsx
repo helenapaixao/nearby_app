d92042cef0b051fe18a4efa89245dcd8af2893d9
helenapaixao <hp.helenapaixao@gmail.com>
2024-12-15 11:52:43 -0300

    feat: fetch categories on Home component mount

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,6 +1,23 @@
-import { View, Text } from "react-native";
+import { useEffect } from "react";
+import { View, Text, Alert } from "react-native";
+import { api } from "@/services/api";
 
 export default function Home() {
+
+  async function fetchCategories() {
+    try {
+      const {data} = await api.get("/categories")
+      console.log(data)
+    } catch(error) {
+      console.log(error)
+      alert("Nao foi possivel carregar as categorias")
+    }
+  }
+
+  useEffect(() => {
+      fetchCategories()
+  },[])
+
   return (
     <View style={{flex: 1}}>
       <Text>Home</Text>
