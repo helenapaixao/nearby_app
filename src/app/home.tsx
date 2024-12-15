f4a32b4ee63c6cca0e418bd8da9dd2b0a567e5d7
helenapaixao <hp.helenapaixao@gmail.com>
2024-12-15 12:53:19 -0300

    feat: Add lazy loading for images and update Home component

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,26 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { View, Text, Alert } from "react-native";
 import { api } from "@/services/api";
+import { Categories } from "@/components/categories";
 
 export default function Home() {
+  const [categories, setCategories] = useState(); 
 
   async function fetchCategories() {
     try {
-      const {data} = await api.get("/categories")
-      console.log(data)
-    } catch(error) {
-      console.log(error)
-      alert("Nao foi possivel carregar as categorias")
+      const { data } = await api.get("/categories");
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+      alert("Nao foi possivel carregar as categorias");
     }
   }
 
   useEffect(() => {
-      fetchCategories()
-  },[])
+    fetchCategories();
+  }, []);
 
   return (
-    <View style={{flex: 1}}>
-      <Text>Home</Text>
+    <View style={{ flex: 1 }}>
+      <Categories data={categories} />
     </View>
-  )
+  );
 }
